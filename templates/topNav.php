0b2ee0676b475fc5c8077b04ad2a83d4c6500c2f
Dawid Wojdyła <dawidw23@poczta.onet.pl>
2019-03-21 19:58:15 +0100

    Corrected a typo

--- templates/topNav.php
+++ templates/topNav.php
@@ -12,7 +12,7 @@ function showTopNav() {
 	<a href="index.php?action=showMenu"><span class='glyphicon glyphicon-home nav-icon'></span>Strona Główna</a>
 	<a href="index.php?action=showIncomeAddingForm"><span class='glyphicon glyphicon-plus nav-icon'></span>Dodaj Przychód</a>
 	<a href="index.php?action=showExpenseAddingForm"><span class='glyphicon glyphicon-minus nav-icon'></span>Dodaj Wydatek</a>
-	<a href="index.php?action=showBalance"><span class='glyphicon glyphicon-stats nav-icon'></span>Przeglądaj Blans</a>
+	<a href="index.php?action=showBalance"><span class='glyphicon glyphicon-stats nav-icon'></span>Przeglądaj Bilans</a>
 	<a href="index.php?action=showSettings"><span class='glyphicon glyphicon-cog nav-icon'></span>Ustawienia</a>
 	<a href="javascript:void(0);" class="hamburger" onclick="showTopNav()">
 		<span class='glyphicon glyphicon-menu-hamburger login-icon' style="font-size: 20px;"></span>
